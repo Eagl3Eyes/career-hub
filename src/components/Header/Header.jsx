3469bdaedba35aecd67f8c7e21975cf426391f06
Eagl3Eyes <58110053+Eagl3Eyes@users.noreply.github.com>
2023-04-12 00:30:23 +0600

    database added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,13 @@ import { Bars3Icon } from '@heroicons/react/24/solid';
 const Header = () => {
     return (
         <div>
-            <div className="navbar bg-base-100 px-5 md:px-28">
+            <div className="navbar md:w-[80%] md:mx-auto bg-base-100 px-5">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex="0" className="lg:hidden">
                             <Bars3Icon className=" h-6 w-6" />
                         </label>
-                        <ul tabIndex="0" className=" menu-compact dropdown-content mt-3 p-2 bg-yellow-100 rounded-box w-52">
+                        <ul tabIndex="0" className="menu-compact dropdown-content mt-3 p-2 bg-yellow-100 rounded-box w-52">
                             <li><Link to="">Home</Link></li>
                             <li><Link to="/statistics">Statistics</Link></li>
                             <li><Link to="/appliedjobs">Applied Jobs</Link></li>
@@ -24,14 +24,14 @@ const Header = () => {
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu-horizontal px-1 gap-5">
-                        <li className='m-3 lg:m-0'><Link to="">Home</Link></li>
-                        <li className='m-3 lg:m-0'><Link to="/statistics">Statistics</Link></li>
-                        <li className='m-3 lg:m-0'><Link to="/appliedjobs">Applied Jobs</Link></li>
-                        <li className='m-3 lg:m-0'><Link to="/blog">Blog</Link></li>
+                        <li className='m-3 lg:m-0 font-bold hover:text-purple-700'><Link to="">Home</Link></li>
+                        <li className='m-3 lg:m-0 font-bold hover:text-purple-700'><Link to="/statistics">Statistics</Link></li>
+                        <li className='m-3 lg:m-0 font-bold hover:text-purple-700'><Link to="/appliedjobs">Applied Jobs</Link></li>
+                        <li className='m-3 lg:m-0 font-bold hover:text-purple-700'><Link to="/blog">Blog</Link></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <button className='px-4 py-3 rounded text-white bg-primary text-sm'>Apply Now</button>
+                    <button className='px-4 py-3 rounded text-white bg-primary hover:bg-primary-focus text-sm'>Apply Now</button>
                 </div>
             </div>
         </div>
