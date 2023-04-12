a45b0d7ec21dabfd796a5dce6a26922d142dec4e
Eagl3Eyes <58110053+Eagl3Eyes@users.noreply.github.com>
2023-04-12 17:36:05 +0600

    bug fixed

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -7,19 +7,19 @@ const Blog = () => {
             
             <div className='grid grid-cols-1 gap-3'>
                 <div className='border-2 rounded-md p-4'>
-                    <h3 className='text-1xl font-semibold py-3'>What is ContextApi ?</h3>
+                    <h3 className='text-1xl font-semibold py-3'>When should you use context API?</h3>
                     <p>Answer: It uses to pass data from parent to children component tree directly, instead of passing it down through a chain of props</p>
                 </div>
                 <div className='border-2 rounded-md p-4'>
-                    <h3 className='text-1xl font-semibold py-3'>What is custom hook in react.js?</h3>
+                    <h3 className='text-1xl font-semibold py-3'>What is custom hook?</h3>
                     <p>Answer: Instead of writing the same code across multiple components that use the same common stateful logic like (“setState”), you can put that code inside a custom hook and reuse it.</p>
                 </div>
                 <div className='border-2 rounded-md p-4'>
-                    <h3 className='text-1xl font-semibold py-3'>What is useRef in react.js?</h3>
+                    <h3 className='text-1xl font-semibold py-3'>What is useRef?</h3>
                     <p>Answer: useRef is used to create a reference object</p>
                 </div>
                 <div className='border-2 rounded-md p-4'>
-                    <h3 className='text-1xl font-semibold py-3'>What is useMemo in react.js?</h3>
+                    <h3 className='text-1xl font-semibold py-3'>What is useMemo?</h3>
                     <p>Answer: It memoizes the calculation result, and returns it to the component.</p>
                 </div>
             </div>
